1266c026bdad7803f6e46afd49a957d1cd7f7b2b
Aurelien Pietrzak <pietrzak.aurelien@gmail.com>
2018-02-28 21:54:46 +0100

    All matrix operations

--- matrix.py
+++ matrix.py
@@ -1,3 +1,6 @@
+import random as rd
+
+
 class Matrix(object):
     """Represent a matrix , mainly to perform matrix operations"""
 
@@ -7,14 +10,52 @@ class Matrix(object):
         self.cols = cols
         self.values = [[0 for _ in range(cols)] for _ in range(rows)]
 
-    def scalar_multiply(self, n):
-        """Mutiply all the values by n"""
+    def randomize(self):
+        """Initialize matriw with random values"""
         for i in range(self.rows):
             for j in range(self.cols):
-                self.values[i][j] *= n
+                self.values[i][j] = int(rd.uniform(0, 10))
 
-    def scalar_addition(self, n):
-        """Addition all the values by n"""
+    def multiply(self, n):
+        """Mutiply all the values by n if n is a value
+        or do the dot product if n if a matrix"""
+
+        if isinstance(n, Matrix):
+            if self.cols != n.rows:
+                print('Columns of A must match rows of B')
+                return
+            else:
+                result = Matrix(n.rows, self.cols)
+                for i in range(self.rows):
+                    for j in range(self.cols):
+                        s = 0
+                        for k in range(self.cols):
+                            s += self.values[i][k] * n.values[k][j]
+                        result.values[i][j] = s
+                return result
+        else:
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    self.values[i][j] *= n
+
+    def addition(self, n):
+        """Add to all the values n if n is a value
+        or addition between m and n if n is a matrix"""
+
+        if isinstance(n, Matrix):
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    self.values[i][j] += n.values[i][j]
+
+        else:
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    self.values[i][j] += n
+
+    def transpose(self):
+        """ Return the transpose of self """
+        result = Matrix(self.cols, self.rows)
         for i in range(self.rows):
             for j in range(self.cols):
-                self.values[i][j] *= n
+                result.values[i][j] = self.values[j][i]
+        return result
